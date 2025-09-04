925a4d69f46f779c179063c1559e2a4fa7b69d8d
zkvn1103@naver.com <zkvn1103@naver.com>
2025-09-04 16:43:17 +0900

    add : chapter 1 실습

---
+++ basic/src/main/java/com/minuk/level02/normal/Application2.java
@@ -0,0 +1,22 @@
+package com.minuk.level02.normal;
+
+public class Application2 {
+
+  public static void main(String[] args) {
+
+    /* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
+     * 총점과 평균을 정수 형태로 출력하세요
+     *
+     * -- 출력 예시 --
+     * 총점 : 201
+     * 평균 : 67
+     *  */
+
+    float koreanLanguageScore = 80.5f;
+    float englishScore = 50.6f;
+    float mathScore = 70.8f;
+
+    System.out.println("총점 : " + (int) (koreanLanguageScore + englishScore + mathScore));
+    System.out.println("평균 : " + (int) ((koreanLanguageScore + englishScore + mathScore) / 3));
+  }
+}
